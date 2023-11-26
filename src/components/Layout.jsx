55ed3f2861911c60c8a19b51a2e4dbeb7075c59f
Ranjit Pandey A00475045 <ranjit.parshuram.pandey@smu.ca>
2023-11-26 00:03:41 -0400

    Text added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
 
     return (<>
         <header className="header">
-            <span className="name-header-container"><p className="name-header">Ranjit Pandey, This is DEV</p></span>
+            <span className="name-header-container"><p className="name-header">This is DEV</p></span>
             <nav className={"nav-container"}>
                 <span className={`nav-element${(activeTab === 'A') ? " active" : ""}`} onClick={() => setActiveTab('A')}><Link to="/" >About me </Link></span>
                 <span className={`nav-element${(activeTab === 'M') ? " active" : ""}`} onClick={() => setActiveTab('M')}><Link to="/mycity" >My City </Link></span>
