e21ab7aece4b24aa8a98c687bad2c0c1b4903063
Ranjit Pandey A00475045 <ranjit.parshuram.pandey@smu.ca>
2023-11-25 23:48:01 -0400

    Text added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from 'react-router-dom';
 import './css/masterCss.css';
 
@@ -9,7 +9,7 @@ const Layout = () => {
 
     return (<>
         <header className="header">
-            <span className="name-header-container"><p className="name-header">Ranjit Pandey</p></span>
+            <span className="name-header-container"><p className="name-header">Ranjit Pandey, This is QA</p></span>
             <nav className={"nav-container"}>
                 <span className={`nav-element${(activeTab == 'A') ? " active" : ""}`} onClick={() => setActiveTab('A')}><Link to="/" >About me </Link></span>
                 <span className={`nav-element${(activeTab == 'M') ? " active" : ""}`} onClick={() => setActiveTab('M')}><Link to="/mycity" >My City </Link></span>
